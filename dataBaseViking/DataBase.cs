d99e29a8a97d89fba04b665267a970aef7e211e6
serega23467 <114952677+serega23467@users.noreply.github.com>
2023-04-02 22:01:24 +0300

    Add files via upload

---
+++ dataBaseViking/DataBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace dataBaseViking
+{
+    public class DataBase
+    {
+        private static DataBase instance;
+        protected DataBase()
+        { }
+        public static DataBase getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DataBase();
+                return instance;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        static string pathToDatabase = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vikingDB.db");
+        SQLiteConnection connection = new SQLiteConnection("Data Source=" + pathToDatabase + ";Version=3; FailIfMissing=True");
+        public SQLiteConnection getConnection()
+        {
+            return connection;
+        }
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public string ToDateSQLite(DateTime value)
+        {
+            string format_date = "yyyy-MM-dd";
+            return value.ToString(format_date);
+        }
+    }
+}
